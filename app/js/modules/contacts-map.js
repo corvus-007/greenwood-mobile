528c84d931f2c11db14e5d96402412d0e477a37d
Denis <corvus007@yandex.ru>
2018-03-06 15:08:47 +0300

    Заменяет гугл карту на яндекс карту в файле contacts-map.js

--- app/js/modules/contacts-map.js
+++ app/js/modules/contacts-map.js
@@ -1,285 +1,38 @@
-window.contactsMap = (function() {
-  var contactsMap = document.querySelector('.contacts__map');
+window.contactsMap = (function () {
+  var contacts = document.querySelector('.contacts');
 
-  if (!contactsMap) {
+  if (!contacts) {
     return;
   }
 
-  function initialize() {
-    geocoder = new google.maps.Geocoder();
-    geocoder.geocode({ address: 'г. Брянск, пр-кт Ленина, д. 67' }, function(
-      results,
-      status
-    ) {
-      if (status == google.maps.GeocoderStatus.OK) {
-        console.log(
-          results[0].geometry.location.G +
-            ' - ' +
-            results[0].geometry.location.K
-        );
-      }
+  ymaps.ready(init);
+
+  var centerMap = [53.250513, 34.371768];
+  var pinSize = [48, 60];
+  var pinOffset = [-24, -60];
+
+  function init() {
+    contactsMap = new window.ymaps.Map("contacts-map", {
+      center: centerMap,
+      zoom: 17,
+      controls: []
     });
 
-    var pyrmont = new google.maps.LatLng(53.25050150762013, 34.37128851179553);
-
-    var stylesMap = [
-      {
-        elementType: 'geometry',
-        stylers: [
-          {
-            color: '#ebe3cd'
-          }
-        ]
-      },
-      {
-        elementType: 'labels.text.fill',
-        stylers: [
-          {
-            color: '#523735'
-          }
-        ]
-      },
-      {
-        elementType: 'labels.text.stroke',
-        stylers: [
-          {
-            color: '#f5f1e6'
-          }
-        ]
-      },
-      {
-        featureType: 'administrative',
-        elementType: 'geometry.stroke',
-        stylers: [
-          {
-            color: '#c9b2a6'
-          }
-        ]
-      },
-      {
-        featureType: 'administrative.land_parcel',
-        elementType: 'geometry.stroke',
-        stylers: [
-          {
-            color: '#dcd2be'
-          }
-        ]
-      },
-      {
-        featureType: 'administrative.land_parcel',
-        elementType: 'labels.text.fill',
-        stylers: [
-          {
-            color: '#ae9e90'
-          }
-        ]
-      },
-      {
-        featureType: 'landscape.natural',
-        elementType: 'geometry',
-        stylers: [
-          {
-            color: '#dfd2ae'
-          }
-        ]
-      },
-      {
-        featureType: 'poi',
-        elementType: 'geometry',
-        stylers: [
-          {
-            color: '#dfd2ae'
-          }
-        ]
-      },
-      {
-        featureType: 'poi',
-        elementType: 'labels.text.stroke',
-        stylers: [
-          {
-            visibility: 'off'
-          }
-        ]
-      },
-      {
-        featureType: 'poi.park',
-        elementType: 'geometry.fill',
-        stylers: [
-          {
-            color: '#77b174'
-          }
-        ]
-      },
-      {
-        featureType: 'poi.park',
-        elementType: 'labels.text.fill',
-        stylers: [
-          {
-            color: '#447530'
-          }
-        ]
-      },
-      {
-        featureType: 'road',
-        elementType: 'geometry',
-        stylers: [
-          {
-            color: '#f5f1e6'
-          }
-        ]
-      },
-      {
-        featureType: 'road.arterial',
-        elementType: 'geometry',
-        stylers: [
-          {
-            color: '#fdfcf8'
-          }
-        ]
-      },
-      {
-        featureType: 'road.highway',
-        elementType: 'geometry',
-        stylers: [
-          {
-            color: '#f8c967'
-          }
-        ]
-      },
-      {
-        featureType: 'road.highway',
-        elementType: 'geometry.stroke',
-        stylers: [
-          {
-            color: '#e9bc62'
-          }
-        ]
-      },
-      {
-        featureType: 'road.highway.controlled_access',
-        elementType: 'geometry',
-        stylers: [
-          {
-            color: '#e98d58'
-          }
-        ]
-      },
-      {
-        featureType: 'road.highway.controlled_access',
-        elementType: 'geometry.stroke',
-        stylers: [
-          {
-            color: '#db8555'
-          }
-        ]
-      },
-      {
-        featureType: 'road.local',
-        elementType: 'labels.text.fill',
-        stylers: [
-          {
-            color: '#806b63'
-          }
-        ]
-      },
-      {
-        featureType: 'transit.line',
-        elementType: 'geometry',
-        stylers: [
-          {
-            color: '#dfd2ae'
-          }
-        ]
-      },
-      {
-        featureType: 'transit.line',
-        elementType: 'labels.text.fill',
-        stylers: [
-          {
-            color: '#8f7d77'
-          }
-        ]
-      },
-      {
-        featureType: 'transit.line',
-        elementType: 'labels.text.stroke',
-        stylers: [
-          {
-            color: '#ebe3cd'
-          }
-        ]
-      },
-      {
-        featureType: 'transit.station',
-        elementType: 'geometry',
-        stylers: [
-          {
-            color: '#dfd2ae'
-          }
-        ]
-      },
-      {
-        featureType: 'water',
-        elementType: 'geometry.fill',
-        stylers: [
-          {
-            color: '#b9d3c2'
-          }
-        ]
-      },
-      {
-        featureType: 'water',
-        elementType: 'labels.text.fill',
-        stylers: [
-          {
-            color: '#92998d'
-          }
-        ]
-      }
-    ];
-
-    var map = new google.maps.Map(document.getElementById('map'), {
-      center: pyrmont,
-      zoom: 16,
-      scrollwheel: true,
-      mapTypeControl: true,
-      mapTypeControlOptions: {
-        style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
-        position: google.maps.ControlPosition.TOP_CENTER
-      },
-      scaleControl: true,
-      streetViewControl: true,
-      streetViewControlOptions: {
-        position: google.maps.ControlPosition.RIGHT_CENTER
-      },
-      zoomControl: true,
-      zoomControlOptions: {
-        position: google.maps.ControlPosition.RIGHT_CENTER
-      }
+    contactsMap.controls.add('zoomControl', {
+      size: 'small',
+      zoomDuration: 400
     });
 
-    map.setOptions({ styles: stylesMap });
-
-    var image = {
-      url: 'images/contacts_icon.png',
-      // This marker is 20 pixels wide by 32 pixels high.
-      size: new google.maps.Size(82, 104),
-      // The origin for this image is (0, 0).
-      origin: new google.maps.Point(0, 0),
-      // The anchor for this image is the base of the flagpole at (0, 32).
-      anchor: new google.maps.Point(46, 104)
-    };
-
-    // Create a marker and set its position.
-    var marker = new google.maps.Marker({
-      map: map,
-      position: pyrmont,
-      title: 'Hello World!',
-      icon: image
+    districtPin = new ymaps.Placemark(centerMap, {
+      balloonContent: 'г. Брянск, пр-кт Ленина, д. 67'
+    }, {
+      iconLayout: 'default#image',
+      iconImageHref: 'images/contacts_icon.png',
+      iconImageSize: pinSize,
+      iconImageOffset: pinOffset
     });
+
+    contactsMap.geoObjects.add(districtPin);
+
   }
-
-  $(window).on('load', function() {
-    initialize();
-  });
 })();
